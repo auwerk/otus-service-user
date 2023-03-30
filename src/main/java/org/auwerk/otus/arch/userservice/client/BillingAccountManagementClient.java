7b179e8a3b10f5254c51805d070a94dfa7c1e463
auwerk <noisefront@yandex.ru>
2023-03-30 17:30:01 +0300

    Billing API client updated to 1.2.x

--- src/main/java/org/auwerk/otus/arch/userservice/client/BillingAccountManagementClient.java
+++ src/main/java/org/auwerk/otus/arch/userservice/client/BillingAccountManagementClient.java
@@ -2,17 +2,18 @@ package org.auwerk.otus.arch.userservice.client;
 
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 
-import org.auwerk.otus.arch.userservice.client.dto.billing.CreateUserAccountRequestDto;
 import org.auwerk.otus.arch.userservice.client.dto.billing.CreateUserAccountResponseDto;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
 import io.smallrye.mutiny.Uni;
 
 @RegisterRestClient(configKey = "billing-service-api")
-public interface BillingServiceClient {
+@Path("/management/account")
+public interface BillingAccountManagementClient {
 
     @POST
-    @Path("/account")
-    Uni<CreateUserAccountResponseDto> createUserAccount(CreateUserAccountRequestDto request);
+    @Path("/{userName}")
+    Uni<CreateUserAccountResponseDto> createUserAccount(@PathParam("userName") String userName);
 }
--- src/main/java/org/auwerk/otus/arch/userservice/service/impl/BillingServiceImpl.java
+++ src/main/java/org/auwerk/otus/arch/userservice/service/impl/BillingServiceImpl.java
@@ -5,8 +5,7 @@ import java.util.UUID;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
-import org.auwerk.otus.arch.userservice.client.BillingServiceClient;
-import org.auwerk.otus.arch.userservice.client.dto.billing.CreateUserAccountRequestDto;
+import org.auwerk.otus.arch.userservice.client.BillingAccountManagementClient;
 import org.auwerk.otus.arch.userservice.service.BillingService;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
@@ -17,11 +16,11 @@ public class BillingServiceImpl implements BillingService {
 
     @Inject
     @RestClient
-    BillingServiceClient billingServiceClient;
+    BillingAccountManagementClient billingServiceClient;
 
     @Override
     public Uni<UUID> createUserAccount(String userName) {
-        return billingServiceClient.createUserAccount(new CreateUserAccountRequestDto(userName))
+        return billingServiceClient.createUserAccount(userName)
                 .map(response -> response.getAccountId());
     }
 }
